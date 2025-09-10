470c83a9e13a8c221a3baf568805013e259ddc26
louaykahouli <louay.kahouli@ensit.u-tunis.tn>
2025-09-10 01:17:54 +0100

    new daily 2

---
+++ LeetCode/daily/1304.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int j = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            result[j++] = i;
+            result[j++] = -i;
+        }
+
+        if (n % 2 == 1) {
+            result[j] = 0;
+        }
+
+        return result;
+    }
+}
